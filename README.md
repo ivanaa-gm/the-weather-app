44d0af4b90123d07d87c02bfc201ad28caa1f18e
ivanaa-gm <iwanaminowa15@gmail.com>
2025-09-10 12:31:41 +0200

    finishing touches - a readme and a info note :)

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# the-weather-app
+# The Weather App
+☀️🌤️⛅🌥️🌦️🌧️⛈️🌩️🌨️❄️🌫️💨🌪️🌈
+
+This is a modern and visually appealing weather application built with **React** on the frontend and **Node.js + NestJS** on the backend. The app displays current weather and a weather forecast for the next 6 days, displaying the weekly weather in total. There is also astronomical data for the moon phases and the current star sign, daylight data such as sunrise, sunset and day duration and a lot of weather information which is displayed with easily understandable icons and as little text as possible.
+
+## Features
+
+- **Current Weather**: Fetches and displays thorough real-time weather information.
+- **6-Day Forecast**: Daily forecast including min and max temperature, wind and precipitation.
+- **Moon Phase Data**: Cached to reduce API calls (from Stormglass API) while showing daily moon phases.
+- **Automatic Updates**: Current weather updates every 30 minutes for accurate data.
+- **Interactive UI**: Modern design with SVG icons and animated backgrounds.
+- **Multi-metric Support**: Choose wind speed units (`km/h`, `m/s`, `mph`, `kn`) with a responsive button grid.
+- **Multi-language Support**: Users can choose between Bulgarian, English and Spanish.
+- **Geocoding**: A feature that autocompleted search locations with Open Meteo Geocoding API.
+
+
+## Technologies Used
+
+- **Frontend**: React, JavaScript, Tailwind CSS
+- **Backend**: Node.js, NestJS
+- **HTTP Client**: Axios (with RxJS in NestJS)
+- **APIs**: 
+  - Open Meteo for weather and geocoding data
+  - Stormglass for astronomical data
+  - Weather Icons by Bas for SVG icons
+
+## Architecture
+
+- **Frontend**: React app handles UI, user interactions, and displaying weather data.
+- **Backend**: NestJS server fetches data from external APIs, caches moon phase info to limit API usage, and serves data to the frontend.
+
--- frontend/src/components/tabs/InfoTab.jsx
+++ frontend/src/components/tabs/InfoTab.jsx
@@ -7,17 +7,37 @@ const InfoTab = ({ onClose }) => {
       className="absolute left-12 top-0 bg-black/90 border border-gray-600 rounded-xl shadow-xl p-4 md:w-96 w-[20rem]"
       ref={ref}
     >
-      <p className="text-center text-white">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque quo
-        magnam enim quam non dicta molestiae sequi animi perferendis. Lorem
-        ipsum dolor sit amet consectetur, adipisicing elit. Rerum ullam
-        assumenda sed iste. Obcaecati qui libero earum maiores corrupti unde
-        cupiditate incidunt deleniti aut enim, laboriosam culpa pariatur,
-        distinctio tenetur. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Sunt rerum cum sint numquam nam quas, nulla itaque earum
-        architecto maiores eligendi labore voluptatibus consequatur quam
-        molestiae neque suscipit dolore saepe.
-      </p>
+      <div className="max-h-80 overflow-scroll text-wrap text-white">
+        <p>
+          I built The Weather App mainly because I wanted to explore more
+          creative design elements in a web application. I’ve always enjoyed
+          playing with backgrounds, SVGs and animated icons and creating new
+          ones when I couldn't find what I was looking for. Thanks to the
+          Weather Icons by Bas, I found the perfect set of icons that inspired
+          me to build the entire app.
+        </p>
+        <br></br>
+        <p>
+          From a technical perspective the app is built with React + Tailwind
+          CSS for a smooth and modern user interface and Node.js + NestJS,
+          serving as a proxy for external API calls. The APIs that I am using
+          are Open Meteo for weather forecasts and location geocoding and
+          Stormglass for moon phase and astronomical data. Not only they are
+          free of charge, but also they serve thorough information that helped
+          me make the app useful. The backend fetches current weather updates at
+          half-hour intervals in order to provide accurate and timely
+          information. Since Stormglass API has a limited daily quota, I cache
+          the first day’s moon phase in memory. A new API call is only made if
+          the cached date is no longer current.
+        </p>
+        <br></br>
+        <p>
+          Overall, this app combines both my interest in design and frontend
+          animations with my growing experience in backend development and API
+          integrations. It was a very fun project that helped me to improve both
+          my technical and creative skills.
+        </p>
+      </div>
     </div>
   );
 };
